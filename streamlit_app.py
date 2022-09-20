63511251e469edcafb0e2e0da5b18e17c4ce63b0
bacman1229 <113620863+bacman1229@users.noreply.github.com>
2022-09-20 10:05:23 -0400

    Update streamlit_app.py
    testing snowflake query capability

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,11 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 #display tabular results
 streamlit.dataframe(fruityvice_normalized)
+
+#run snowflake queries
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
